1af064fad4149a9b1c50cb4bf987b3232d404c12
RedSide27 <dupdupont27@gmail.com>
2017-11-02 15:19:20 -0400

    controller categorie et post fait

---
+++ TP_W24/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TP_W24.Models;
+
+namespace TP_W24.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CryptoBDEntities1 db = new CryptoBDEntities1();
+        public ActionResult Index()
+        {
+            return View(db.Categories.ToList());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult categorie()
+        {
+            return View(db.Posts.ToList());
+        }
+
+        public ActionResult post()
+        {
+            return View();
+        }
+
+        public ActionResult login()
+        {
+            return View();
+        }
+
+        public ActionResult register()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TP_W24/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TP_W24.Startup))]
+namespace TP_W24
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
