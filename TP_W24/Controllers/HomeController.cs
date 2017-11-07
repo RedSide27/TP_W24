c68f3c065aebab05e4bc04edb46e0c7732775c04
RedSide27 <dupdupont27@gmail.com>
2017-11-07 15:33:19 -0500

    tout les page se charge a partir de la bd

--- TP_W24/Controllers/HomeController.cs
+++ TP_W24/Controllers/HomeController.cs
@@ -55,9 +55,35 @@ namespace TP_W24.Controllers
             return View(post.ToList());
         }
 
-        public ActionResult post()
+        public ActionResult post(int NoCat)
         {
-            return View();
+            var NoPost = db.Posts.Where(t => t.FK_Categories_ID == NoCat).Select(t => t.Post_ID).FirstOrDefault();
+            var comment = (from c in db.Comments.Where(t=> t.FK_Post_ID == NoPost)
+                           select new CommentDisplay
+                           {
+                               CommentID = c.CommentID,
+                               Comment_Text = c.Comment_Text,
+                               Comment_Date_Heure = c.Comment_Date_Heure,
+                               Post_ID = c.FK_Post_ID,
+                               Username = db.AspNetUsers.Where(t => t.Id == c.FK_User_ID).Select(t => t.UserName).FirstOrDefault(),
+                               User_PATH_IMG = db.AspNetUsers.Where(t => t.Id == c.FK_User_ID).Select(t => t.UserPath_Img).FirstOrDefault(),
+                               post = (from p in db.Posts.Where(t => t.FK_Categories_ID == NoCat)
+                                           select new PostDisplay
+                                           {
+                                               PostID = p.Post_ID,
+                                               PostName = p.Post_Name,
+                                               PostMessage = p.Post_Message,
+                                               PostDateHeure = p.Post_Date_Heure,
+                                               PostCategorieID = p.FK_Categories_ID,
+                                               PostUserID = p.FK_User_ID,
+                                               UserPathImg = db.AspNetUsers.Where(t => t.Id == p.FK_User_ID).Select(t => t.UserPath_Img).FirstOrDefault(),
+                                               UserName = db.AspNetUsers.Where(t => t.Id == p.FK_User_ID).Select(t => t.UserName).FirstOrDefault(),
+                                               NbComment = db.Comments.Where(t => t.FK_Post_ID == p.Post_ID).Count(),
+                                               CategorieName = db.Categories.Where(t => t.Categorie_ID == p.FK_Categories_ID).Select(t => t.CategorieName).FirstOrDefault()
+                                           }).ToList()
+                            }).ToList();
+
+            return View(comment.ToList());
         }
 
         public ActionResult login()
@@ -70,6 +96,18 @@ namespace TP_W24.Controllers
             return View();
         }
 
+    }
+
+    public class CommentDisplay
+    {
+        [Key, Column(Order = 0)]
+        public int CommentID { get; set; }
+        public string Comment_Text { get; set; }
+        public DateTime Comment_Date_Heure { get; set; }
+        public int Post_ID { get; set; }
+        public string Username { get; set; }
+        public string User_PATH_IMG { get; set; }
+        public List<PostDisplay> post { get; set; }
     }
 
     public class PostDisplay
